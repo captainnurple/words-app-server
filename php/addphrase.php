954ca8ef0c3b08ca8f96a198fa0ea63cbfdffa76
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-17 21:36:57 -0700

    addphrase initial test

--- php/addphrase.php
+++ php/addphrase.php
@@ -1,3 +1,50 @@
+<!DOCTYPE HTML> 
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body> 
+
 <?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+       $nameErr = "Only letters and white space allowed"; 
+     }
+   }
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
 
 ?>
+
+<h2>Send New Love Phrase</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   Phrase: <input type="text" name="name" value="<?php echo $name;?>">
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+
+<?php
+echo "<h2>Successfully Input:</h2>";
+echo $name;
+echo "<br>";
+?>
+
+</body>
+</html>
