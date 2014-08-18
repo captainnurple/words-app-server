f01e591d05b009286d1a1acbae64cd148d10d0d4
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-17 21:58:28 -0700

    Incremental

--- php/addphrase.php
+++ php/addphrase.php
@@ -34,7 +34,6 @@ function test_input($data) {
 ?>
 
 <h2>Send New Love Phrase</h2>
-<p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
    Phrase: <input type="text" name="name" value="<?php echo $name;?>">
    <span class="error">* <?php echo $nameErr;?></span>
