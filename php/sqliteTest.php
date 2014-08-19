29cb7c83da55af88b3b39612e605edd4aed010d9
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-18 19:30:49 -0700

    Incremental

--- php/sqliteTest.php
+++ php/sqliteTest.php
@@ -12,4 +12,25 @@
    } else {
       echo "Opened database successfully\n";
    }
+   
+$query = <<<EOD
+  CREATE TABLE IF NOT EXISTS phrases (
+    phrase TEXT)
+EOD;
+
+    $db->exec($query) or die('Create db failed');
+    $phrase = sanitize($_POST['Oh hi baby I love you!!']);
+
+$query = <<<EOD
+  INSERT INTO users VALUES ( '$phrase')
+EOD;
+
+    $db->exec($query) or die("Unable to add phrase $phrase");
+    $result = $db->query('SELECT * FROM phrases') or die('Query failed');
+
+    while ($row = $result->fetchArray())
+    {
+      echo "Phrase: {$row['phrase']}\n";
+    }
+
 ?>
