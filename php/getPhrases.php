c65b16a7b7f6d0c38bf8358ebd261e866811414d
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-23 16:48:23 -0700

    Incremental

--- php/getPhrases.php
+++ php/getPhrases.php
@@ -17,7 +17,7 @@
 	$dbh = new PDO($dir) or die("cannot open database");
 
     // construct db query filtering for index great than last received
-	$query = "SELECT * FROM phrases";
+	$query = "SELECT (rowid, phrase) FROM phrases";
 
     // test for content in json. if none return appropriate result
 
