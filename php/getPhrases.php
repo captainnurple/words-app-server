5a2bcd52fcd27081bb086575e0e9be15d0cdb366
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-23 16:18:56 -0700

    Incremental

--- php/getPhrases.php
+++ php/getPhrases.php
@@ -1,9 +1,5 @@
-<?php
-    echo $_GET['latest'] . PHP_EOL; 
-    echo $_GET['key'] . PHP_EOL; 
-    
+<?php    
     $key = $_GET['key'];
-    echo $key . PHP_EOL;
     
     if ($key !== '6ForCM1pN370iAzDYTKXZIk47SlH3Yxu') {
         echo 'die now';
