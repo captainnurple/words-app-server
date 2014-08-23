92733fdcc5112743c41658ec0a5c5042c6773fd2
Malcolm Home MBP <malcolm.murdock@gmail.com>
2014-08-23 16:53:14 -0700

    Incremental

--- php/getPhrases.php
+++ php/getPhrases.php
@@ -24,7 +24,7 @@
     // dump results into appropriate json and ret 
     	// Iterate through the results and pass into JSON encoder //
 	foreach ($dbh->query($query) as $row) {
-		echo json_encode(array($row['rowid'], $row['phrase']);
+		echo json_encode(array($row['rowid'], $row['phrase']));
 	}
 
 ?>
